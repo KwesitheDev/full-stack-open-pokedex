355433a550824cc5684b58805ed8614e40fcc092
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2025-10-21 14:18:07 +0000

    simulated broken deployment

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.get('/health', (req, res) => {
-  res.send('Ok')
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
+  res.send('ok')
 })
 
 
